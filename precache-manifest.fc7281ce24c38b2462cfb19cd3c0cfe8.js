2eda5fbf0693bff3070119ea92702ca07f292b8c
LiweniMac <554052409@qq.com>
2019-04-07 23:38:01 +1000

    Updates

---
+++ precache-manifest.fc7281ce24c38b2462cfb19cd3c0cfe8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "babc83ea71f624a2c10cac458af0947c",
+    "url": "/tplanDeployV5/static/media/teamlogo_white.babc83ea.png"
+  },
+  {
+    "revision": "793ad8e8871be579395b185126a6f0ea",
+    "url": "/tplanDeployV5/static/media/planner2.793ad8e8.jpg"
+  },
+  {
+    "revision": "c59ce5249f0247cf33fff15fdffea569",
+    "url": "/tplanDeployV5/static/media/image-3.c59ce524.jpg"
+  },
+  {
+    "revision": "e2dc0ca7b7e33c30f812caadff6d6aef",
+    "url": "/tplanDeployV5/static/media/image-2.e2dc0ca7.jpg"
+  },
+  {
+    "revision": "6a702905aea28bec47b115ccf680cb01",
+    "url": "/tplanDeployV5/static/media/image-1.6a702905.jpg"
+  },
+  {
+    "revision": "c82c323b54ee925963d3",
+    "url": "/tplanDeployV5/static/js/runtime~main.09bd848a.js"
+  },
+  {
+    "revision": "6157847133b12f4564cd",
+    "url": "/tplanDeployV5/static/js/main.85d83b75.chunk.js"
+  },
+  {
+    "revision": "55c898bec4c2a6e8051d",
+    "url": "/tplanDeployV5/static/js/2.0bb6bc3b.chunk.js"
+  },
+  {
+    "revision": "6157847133b12f4564cd",
+    "url": "/tplanDeployV5/static/css/main.aa0abc21.chunk.css"
+  },
+  {
+    "revision": "29a1543107b90007d0016f9012eed639",
+    "url": "/tplanDeployV5/index.html"
+  }
+];
